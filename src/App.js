68ef1743734b95e1e6e4e9a253ea9bd9c629039e
ilija01291 <ilija01291@gmail.com>
2020-08-11 11:57:27 +0200

    force push

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import "./App.css";
+
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>completed task</button>
+        <button onClick={() => removeTodo(index)}>remove</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Enter a new task"
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Shopping",
+      isCompleted: false
+    },
+    {
+      text: "Go for computer in service",
+      isCompleted: false
+    },
+    
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos,  { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm  addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
